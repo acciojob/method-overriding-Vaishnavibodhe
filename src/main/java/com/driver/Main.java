3ca6a91b60a23d606394ce49514daf65f2513e04
Vaishnavibodhe <128290200+Vaishnavibodhe@users.noreply.github.com>
2023-07-13 15:05:36 +0530

    code changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
-public class Main {
-  
+  public static class B extends A {
+    public void meth(){
+               System.out.println("Method is overrridden in extended class B");
+
+  }
+           public static void main(String[] args){
+               B obj=new B();
+              obj.meth();
+
+             B obj1=new B();
+             obj1.meth();
+    }
 }
