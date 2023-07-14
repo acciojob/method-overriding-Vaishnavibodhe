c25292644165a781904ab04bc0d130f6df13d009
Vaishnavibodhe <128290200+Vaishnavibodhe@users.noreply.github.com>
2023-07-14 12:39:23 +0530

    code changes by me

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,14 +4,10 @@ package com.driver;
 public class Main{
     public static class A {
 
-        public String meth() {
-            return "Invoking method from class A";
-        }
     }
     public static class B extends A{
-        public String meth(){
+        private void meth(){
 
-            return "Method is overridden Extendend class B";
         }
     }
     public static void main(String[] args){
